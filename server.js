9b9ce761e5fe785e7b59645e32a77403fc6e1ded
Gabor Szabo1 <gabor_szabo1@epam.com>
2024-01-28 19:09:22 +0100

    fix frontend

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 app.use("/api/v1/auth", authRouter);
 
@@ -42,7 +42,7 @@ app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 app.use("*", (req, res) => {
